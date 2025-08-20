18cb3abe6931ca4653192239163943f2f102fd4c
Kay <kamilymilena16@gmail.com>
2025-08-20 20:37:10 -0300

    ProjetoAluno

--- ProjetoAluno.java
+++ ProjetoAluno.java
@@ -7,15 +7,25 @@ public static void main (String[] arg) {
 	
 	obj1.nome = "Milena Kamily Aragão de Souza";
 	obj1.matricula = 4180528;
-	obj1.notaAv1 = 5;
+	obj1.notaAv1 = 7;
 	obj1.notaAv2 = 7;
 	obj1.calcMedia();
+	
+	obj2.nome = "Fulano de tal";
+	obj2.matricula = 4180528;
+	obj2.notaAv1 = 5;
+	obj2.notaAv2 = 5;
+	obj2.calcMedia();
 	
 	System.out.print("Nome: " + obj1.nome);
 	System.out.print("Matricula" + obj1.matricula);
 	System.out.print("Sua nota da 1 Avaliação é:  " + obj1.notaAv1);
 	System.out.print("Sua nota da 2 Avaliação é: " + obj1.notaAv2);
 	
+	System.out.print("Nome: " + obj2.nome);
+	System.out.print("Matricula" + obj2.matricula);
+	System.out.print("Sua nota da 1 Avaliação é:  " + obj2.notaAv1);
+	System.out.print("Sua nota da 2 Avaliação é: " + obj2.notaAv2);
 	
 	
 	
